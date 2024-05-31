fe5f3d97ff33325d22e6277a3839cc8a6025c2ff
Dricmoy Bhattacharjee <dricbhattacharjee@gmail.com>
2024-05-31 06:50:55 -0600

    Current go backend moved to Azure, new go backend needed for university check

--- noteapp/src/app/page.tsx
+++ noteapp/src/app/page.tsx
@@ -48,33 +48,35 @@ const SearchBar = () => {
   };
 
   return (
-    <><Nav page_name={'university'}></Nav>
+    <>
+      <Nav page_name={'university'}></Nav>
     
-    <div className="relative w-full max-w-md mx-auto">
-      <form onSubmit={handleSubmit} className="flex items-center">
-        <Input
-          type="text"
-          value={query}
-          onChange={handleChange}
-          placeholder="Enter an university"
-          className="flex-1 px-4 py-2 border rounded-l-lg" />
-        <Button variant="default">Search</Button>
-      </form>
-      {suggestions.length > 0 && (
-        <ul className="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-lg shadow-lg">
-          {suggestions.map((suggestion) => (
-            <li
-              key={suggestion}
-              className="px-4 py-2 cursor-pointer hover:bg-gray-100"
-              onClick={() => handleSuggestionClick(suggestion)}
-            >
-              {suggestion}
-            </li>
-          ))}
-        </ul>
-      )}
-      {notFound && <div className="mt-2 text-primary-500">University not found</div>}
-    </div></>
+      <div className="relative w-full max-w-md mx-auto">
+        <form onSubmit={handleSubmit} className="flex items-center">
+          <Input
+            type="text"
+            value={query}
+            onChange={handleChange}
+            placeholder="Enter a university"
+            className="flex-1 px-4 py-2 border rounded-l-lg" />
+          <Button variant="default">Search</Button>
+        </form>
+        {suggestions.length > 0 && (
+          <ul className="absolute z-10 w-full mt-1 bg-white border border-gray-300 rounded-lg shadow-lg">
+            {suggestions.map((suggestion) => (
+              <li
+                key={suggestion}
+                className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                onClick={() => handleSuggestionClick(suggestion)}
+              >
+                {suggestion}
+              </li>
+            ))}
+          </ul>
+        )}
+        {notFound && <div className="mt-2 text-primary-500">University not found</div>}
+      </div>
+    </>
   );
 };
 
